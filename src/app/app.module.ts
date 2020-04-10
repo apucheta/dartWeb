532d92e5e917af81d02666cbc16d5bdbe90558d5
Luis Ontiveros <leomkr@gmail.com>
2020-04-10 08:39:55 -0300

    Se agregar FormsModule para que se pueda usar ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { MarcasComponent } from './pages/marcas/marcas.component';
       { path: '', component: HomeComponent },
       { path: 'calculo', component: CalculoComponent },
       { path: 'marcas', component: MarcasComponent },
-    ])
+    ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
