6002fb8f8d0a402a3f2d8a319826d59688743686
Agustin Pucheta <aguspucheta24293@gmail.com>
2020-05-25 02:04:30 -0300

    se agrega ABM de contenidos en la seccion de backoffice

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyATDekr8YBtLhN2P9sszYMgxuAmUvRLTnQ",
-    authDomain: "dart-64259.firebaseapp.com",
-    databaseURL: "https://dart-64259.firebaseio.com",
-    projectId: "dart-64259",
-    storageBucket: "dart-64259.appspot.com",
-    messagingSenderId: "1056907310030",
-    appId: "1:1056907310030:web:8cf9103407c0eaee40b19f",
-    measurementId: "G-N0198GV0EZ"
   }
 };
 
